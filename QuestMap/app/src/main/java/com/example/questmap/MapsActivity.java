253e9c572c437c274a16d4985432b09dacf7f00e
Bedirhan Güner <bedirhanguner@hotmail.com>
2021-11-02 22:07:29 +0300

    Showing user's location at the opening.

--- QuestMap/app/src/main/java/com/example/questmap/MapsActivity.java
+++ QuestMap/app/src/main/java/com/example/questmap/MapsActivity.java
@@ -1,8 +1,23 @@
 package com.example.questmap;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,11 +26,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.questmap.databinding.ActivityMapsBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    LocationListener locationListener;
+    LocationManager locationManager;
+    SharedPreferences sharedPreferences;
+    ActivityResultLauncher<String> permissionLauncher;
+    boolean info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +49,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        sharedPreferences = MapsActivity.this.getSharedPreferences("com.example.questmap",MODE_PRIVATE);
+        info = false;
+
+        registerLauncher();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+                info = sharedPreferences.getBoolean("info",false);
+
+                if(!info){
+                    LatLng userLocation= new LatLng(location.getLatitude(),location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15));
+                    //mMap.addMarker(new MarkerOptions().position(userLocation).title("You are here!"));
+                    sharedPreferences.edit().putBoolean("info",true).apply();
+                }
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(binding.getRoot(),"Permission is needed for using Maps.", Snackbar.LENGTH_INDEFINITE).setAction("GivePermission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                    }
+                }).show();
+            }
+            else{
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+            Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if(lastLocation != null){
+                LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15));
+            }
+        }
+    }
+
+    private void registerLauncher(){
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    if(ContextCompat.checkSelfPermission(MapsActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                        if(lastLocation != null){
+                            LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15));
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(MapsActivity.this,"Permission is needed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
